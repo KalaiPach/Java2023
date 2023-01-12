63273a398fe68748aca2522bf8b81a88f64bcd73
KalaiPach <121917214+KalaiPach@users.noreply.github.com>
2023-01-12 09:45:50 +0530

    update2

---
+++ src/day03/program02.java
@@ -0,0 +1,24 @@
+package day03;
+
+public class program02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=20;
+		double z=10.0;
+		double a=10.2;
+		
+		System.out.println(x>y);
+		System.out.println(y>x);
+		System.out.println(y>=y);
+		System.out.println(x<y);
+		System.out.println(x==z);
+		System.out.println(x==a);
+		System.out.println(x!=y);
+		System.out.println(x!=z);
+		
+
+	}
+
+}

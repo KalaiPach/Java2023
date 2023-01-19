78ae678afa5830cddfefaafb0efaf6cbe5624308
KalaiPach <121917214+KalaiPach@users.noreply.github.com>
2023-01-19 09:31:12 +0530

    update

---
+++ src/day06/program01.java
@@ -0,0 +1,18 @@
+package day06;
+
+public class program01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+		int b = 200;
+		
+		if (a>b)
+		{
+			System.out.println("Good Day");
+			System.out.println("Good Day");
+		}
+		
+	}
+
+}

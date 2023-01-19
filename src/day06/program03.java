32111db47863ea7690afafa3a086d554726040d3
KalaiPach <121917214+KalaiPach@users.noreply.github.com>
2023-01-19 09:42:18 +0530

    updateprog3

---
+++ src/day06/program03.java
@@ -0,0 +1,17 @@
+package day06;
+
+public class program03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=1011;
+		if (x%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	
+	}
+
+}

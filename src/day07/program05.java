b913b89f8cff5203a0462a35be63a8a50807de4e
KalaiPach <121917214+KalaiPach@users.noreply.github.com>
2023-01-20 09:42:19 +0530

    Day07

---
+++ src/day07/program05.java
@@ -0,0 +1,38 @@
+package day07;
+
+public class program05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x="WEN";
+		
+		switch(x) {
+		case "SUN":
+		System.out.println("Day1");
+		break;
+		case "MON":
+		System.out.println("Day2");
+		break;
+		case "TUE":
+		System.out.println("Day 3");
+		break;
+		case "WEN":
+		System.out.println("Day 4");
+		break;
+		case "THU":
+			System.out.println("Day 5");
+			break;
+		case "FRI":
+			System.out.println("Day 6");
+			break;
+		case "SAT":
+			System.out.println("Day 7");
+			break;
+		default:
+			System.out.println("invalid Selection");
+			break;
+		}
+		
+	}
+
+}

752ac6e2077689af95135b05c91e77aa3947b759
KalaiPach <121917214+KalaiPach@users.noreply.github.com>
2023-01-21 09:31:19 +0530

    day 8

---
+++ src/day07/program02.java
@@ -0,0 +1,31 @@
+package day07;
+
+public class program02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=1;
+		while(x<=5) {
+			System.out.println("Good Day!");
+			x=x+1;
+		}
+		
+		int y=-3;
+		while(y<=-1) {
+			System.out.println("Good Morning!");
+			y=y+1;
+		}
+		
+		double a=0.1;
+		while(a<=0.5) {
+		System.out.println("Great Day!");
+		a=a+0.1;
+	}
+		for (int i = 0; i < 5; i++) {
+			  System.out.println(i);
+			}
+	}
+	
+	
+}
---
+++ src/day08/day08.java
@@ -0,0 +1,23 @@
+package day08;
+
+public class day08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=1;
+		
+		do {
+			System.out.println("Good day!");
+			x=x+1;
+		} while (x<5); 
+		
+		int y=5;
+		
+		do {
+			System.out.println("Good Morning!");
+			y=y-1;
+		}while (x<5);
+	}
+
+}

c2c8f6cdc35e6aea93fa9a9b06afe2530e047de0
KalaiPach <121917214+KalaiPach@users.noreply.github.com>
2023-01-27 10:01:40 +0530

    day12

---
+++ src/day12/program01.java
@@ -0,0 +1,20 @@
+package day12;
+
+public class program01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x[]=new int[5];
+		int y[]= {10,20,30,40,50,60};
+		String z[]= {"abc","bca","cba"};
+		x[0]=11;
+		x[1]=22;
+		x[4]=55;
+		x[2]=33;
+		x[3]=44;
+				
+		System.out.println(x[3]);
+
+	}
+
+}
---
+++ src/day12/program02.java
@@ -0,0 +1,25 @@
+package day12;
+
+public class program02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int y[]= {23,31,45,67,76,43,70,89};
+		System.out.println("lenght of y:-" + y.length);
+		
+		for(int i=0; i<y.length; i++)
+		{
+			System.out.println(y[i]);
+		}
+			
+		System.out.println("***************");
+		int a=0;
+		while(a<y.length)
+		{
+			System.out.println(y[a]);
+			a++;
+		}
+	}
+
+}
